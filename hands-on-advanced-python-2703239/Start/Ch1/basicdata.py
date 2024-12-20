ae74dd261b82d599908f89d7c0c79011efb0e454
Anna Tereshchuk <anna_tereshchuk@epam.com>
2024-12-20 11:23:48 +0000

    Updated basicdata.py with new changes in Ch1

--- hands-on-advanced-python-2703239/Start/Ch1/basicdata.py
+++ hands-on-advanced-python-2703239/Start/Ch1/basicdata.py
@@ -10,12 +10,19 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
 
 # TODO: What was the warmest day in the data set?
 # print(len(weatherdata))
-
+warmday = max(weatherdata, key=lambda x: x['tmax'])
+print(f"The warmest day was {warmday['date']} at {warmday['tmax']} degrees")
 # TODO: What was the coldest day in the data set?
+coldday = min(weatherdata, key=lambda x: x['tmin'])
+print(f"The coldest day was {warmday['date']} at {warmday['tmin']} degrees")
 # pprint.pp(weatherdata[0])
 
 # TODO: How many days had snowfall?
 #empty dict
+snowdays =[day for day in weatherdata if day ['snow']>0]
+print(f"Snow fell on {len(snowdays)}days")
+pprint.pp(snowdays)
+
 years= {}
 
 for d in weatherdata:
