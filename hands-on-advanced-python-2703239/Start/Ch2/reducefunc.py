8a3d6f838d947ea837ebd59def3e12c10eb8b29c
Anna Tereshchuk <anna_tereshchuk@epam.com>
2025-01-15 17:06:05 +0000

    Updated reducefunc

--- hands-on-advanced-python-2703239/Start/Ch2/reducefunc.py
+++ hands-on-advanced-python-2703239/Start/Ch2/reducefunc.py
@@ -1,35 +1,42 @@
-# Example file for Advanced Python: Hands On by Joe Marini
-# Using the reduce function
-
 import json
-
+from functools import reduce
 
 # open the sample weather data file and use the json module to load and parse it
 with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: how much snowfall is in the entire dataset?
-
+total_snowfall = reduce(
+    lambda acc, el: acc + el['snow'],
+    weatherdata,
+    0
+)
+print(f"Total snowfall: {total_snowfall} inches")
 
 # TODO: how much total precipitation is in the entire dataset?
-
+total_precip = reduce(
+    lambda acc, el: acc + el['snow'] + el['prcp'],
+    weatherdata,
+    0
+)
+print(f"Total precipitation: {total_precip} inches")
 
 # TODO: What was the warmest day in which it snowed? Need to find highest 'tmax' for all
 # days where 'snow' > 0
 def warm_snow_day(acc, elem):
-    # return the elem value if the snow amount > 0 and its tmax value is
-    # larger than the tmax value that is in the acc argument
-    pass
+    if elem['snow'] > 0 and (acc is None or elem['tmax'] > acc['tmax']):
+        return elem
+    return acc
 
 # define a "zero" value start date for the reduce function to start with
-start_val = {
-    "date": "1900-01-01",
-    "tmin": 0,
-    "tmax": 0,
-    "prcp": 0.0,
-    "snow": 0.0,
-    "snwd": 0.0,
-    "awnd": 0.0
-}
+start_val = None
 
 # TODO: reduce the data set to the warmest snow day
+warmest_snow_day = reduce(warm_snow_day, weatherdata, start_val)
+
+# Print the result
+if warmest_snow_day:
+    print(f"The warmest day on which it snowed was {warmest_snow_day['date']} with a max temperature of {warmest_snow_day['tmax']} and snowfall of {warmest_snow_day['snow']}")
+else:
+    print("No day with snowfall found in the dataset.")
+
