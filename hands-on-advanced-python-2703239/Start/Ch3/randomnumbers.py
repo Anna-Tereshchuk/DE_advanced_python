fb574d558c648c94a21c7c6889ff8ae7158fca3f
Anna Tereshchuk <anna_tereshchuk@epam.com>
2025-01-16 16:50:33 +0000

    Updated randomnumbers

--- hands-on-advanced-python-2703239/Start/Ch3/randomnumbers.py
+++ hands-on-advanced-python-2703239/Start/Ch3/randomnumbers.py
@@ -8,13 +8,14 @@ import random
 with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
-# TODO: the random module can be used to generate random values
+# the random module can be used to generate random values
+print(random.random())  # Генерація випадкового числа з плаваючою точкою між 0 і 1
 
+# choose a random number in a range including both points
+print(random.randint(10, 20))  # Включає обидва кінцеві значення
 
-# TODO: choose a random number in a range including both points
-
-# TODO: choose a random number in a range excluding end point
-
+# choose a random number in a range excluding end point
+print(random.randrange(10, 20))  # Виключає кінцеве значення
 
 # build a list of the summer days in 2019
 def is_summer_day(d):
@@ -24,7 +25,14 @@ def is_summer_day(d):
     return False
 summer_2019 = list(filter(is_summer_day, weatherdata))
 
-# TODO: choose 5 random days from that summer
+# choose 5 random days from that summer
+random.seed(10)  # Встановлення значення seed для контролю випадкових чисел
+random_days = random.sample(summer_2019, 5)
 
+# what was the windiest of those 5 days?
+windiest_day = max(random_days, key=lambda day: day.get('wind_speed', day.get('awnd', 0)))
 
-# TODO: what was the windiest of those 5 days?
+# Виведення результату
+pprint.pprint(random_days)
+print("Найвітряніший день:", windiest_day)
+
